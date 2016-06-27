69198ad67ffd49f6a18fc9ab8d0cff4bcc7e3e25
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-06-27 15:34:49 -0700

    Add function to change logo color upon click

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("I'm loaded!")
+
+
+function changeColor(event) {
+  event.target.style.color="#a93d3d";
+};
+
+var logo = document.getElementById("logo");
+logo.addEventListener("click", changeColor);
